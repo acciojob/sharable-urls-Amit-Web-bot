d349b1d355f5217fc8a35bac572afd9dd7f13899
Amit <84635639+Amit-Web-bot@users.noreply.github.com>
2024-01-08 08:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // your code here
+// let name = document.getElementById("name");
+// let year = document.getElementById("year");
+let btn = document.getElementById("button");
+
+btn.addEventListener("click",(event)=>{
+	event.preventDefault();
+	let name = document.getElementById("name").value;
+	let year = document.getElementById("year").value;
+	if(name!="" && year!=""){
+		document.getElementById("url").innerText = `https://localhost:8080/?name=${name}&year=${year}`
+	}
+	if(name=="" && year!=""){
+		document.getElementById("url").innerText = `https://localhost:8080/?year=${year}`
+	}
+	if(name!="" && year==""){
+		document.getElementById("url").innerText = `https://localhost:8080/?name=${name}`
+	}
+})
